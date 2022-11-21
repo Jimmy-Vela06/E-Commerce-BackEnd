9babca67c558409822627aa5c50901f9dd8bf80e
Jimmy Vela <jimmyvela06@.com>
2022-11-21 00:59:21 -0600

    module changes

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
 const sequelize = require("../config/connection");
+const Category = require("./Category");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,7 +10,6 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -17,32 +17,32 @@ Product.init(
       autoIncrement: true,
     },
     product_name: {
-      type: DataTypes.STRING,
+      tyepe: DataTypes.STRING,
       allowNull: false,
     },
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
-        isDecimal: true,
+        isDeciaml: true,
       },
     },
     stock: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.NUMBER,
       allowNull: false,
-      defaultValue: 10,
-      Validate: {
+      validate: {
         isNumeric: true,
       },
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "category",
-        key: "id",
+      category_id: {
+        type: DataTypes.INTEGER,
+        refrence: {
+          module: "Category",
+          key: "id",
+        },
       },
     },
   },
+
   {
     sequelize,
     timestamps: false,
